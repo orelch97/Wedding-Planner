ffac8bd66ebfa5f200f396cf4fc8d82889adf35b
orelch97 <orelch97@gmail.com>
2026-08-10 22:30:23 +0300

    ניקוי שאריות QA: הסרת קבצי הבדיקה מ-tmp והוספתו ל-gitignore

--- tmp/qa-reset.mjs
+++
@@ -1,6 +0,0 @@
-﻿import { loadEnv } from "../server/env.mjs";
-loadEnv();
-const { createPasswordReset } = await import("../server/auth.js");
-const r = await createPasswordReset("qa-date@example.test");
-console.log("TOKEN=" + (r ? r.token : "NULL"));
-process.exit(0);
